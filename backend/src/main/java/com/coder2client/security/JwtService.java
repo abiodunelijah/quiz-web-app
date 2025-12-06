69be678e614a9e84f34d20c961b4ed4656e91536
sir-abbey <kaybiel4u@gmail.com>
2025-12-06 09:00:12 +0100

    Adding Authentication and Authorization.

--- backend/src/main/java/com/coder2client/security/JwtService.java
+++ backend/src/main/java/com/coder2client/security/JwtService.java
@@ -36,7 +36,7 @@ public class JwtService {
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parserBuilder()
+            Jwts.parser()
                     .setSigningKey(getSigningKey())
                     .build()
                     .parseClaimsJws(token);
@@ -47,7 +47,7 @@ public class JwtService {
     }
 
     public String extractUsername(String token) {
-        Claims claims = Jwts.parserBuilder()
+        Claims claims = Jwts.parser()
                 .setSigningKey(getSigningKey())
                 .build()
                 .parseClaimsJws(token)
